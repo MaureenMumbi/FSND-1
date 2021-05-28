b260af83a7b9690f17f19a9f34619c4eb10f6e98
maureenmumbi <maureen.mumbi@busaracenter.org>
2021-05-28 23:16:53 +0300

    update after review

--- projects/02_trivia_api/starter/backend/README.md
+++ projects/02_trivia_api/starter/backend/README.md
@@ -109,3 +109,337 @@ createdb trivia_test
 psql trivia_test < trivia.psql
 python test_flaskr.py
 ```
+
+
+### Endpoints 
+#### GET /questions
+- General:
+    - Returns a list of question objects, success value, and total number of questions, current_category, and categories 
+    - Results are paginated in groups of 10. Include a request argument to choose page number, starting from 1. 
+- Sample: `curl http://127.0.0.1:5000/questions`
+
+``` 
+{
+
+    "categories": {
+        "1": "Science",
+        "2": "Art",
+        "3": "Geography",
+        "4": "History",
+        "5": "Entertainment",
+        "6": "Sports",
+        "7": "Test"
+    },
+    "current_category": null,
+    "questions": [
+        {
+            "answer": "Apollo 13",
+            "category": 5,
+            "difficulty": 4,
+            "id": 2,
+            "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?",
+            "rating": null
+        },
+        {
+            "answer": "Tom Cruise",
+            "category": 5,
+            "difficulty": 4,
+            "id": 4,
+            "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?",
+            "rating": null
+        },
+        {
+            "answer": "Maya Angelou",
+            "category": 4,
+            "difficulty": 2,
+            "id": 5,
+            "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?",
+            "rating": null
+        },
+        {
+            "answer": "Edward Scissorhands",
+            "category": 5,
+            "difficulty": 3,
+            "id": 6,
+            "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?",
+            "rating": null
+        },
+        {
+            "answer": "Muhammad Ali",
+            "category": 4,
+            "difficulty": 1,
+            "id": 9,
+            "question": "What boxer's original name is Cassius Clay?",
+            "rating": null
+        },
+        {
+            "answer": "Brazil",
+            "category": 6,
+            "difficulty": 3,
+            "id": 10,
+            "question": "Which is the only team to play in every soccer World Cup tournament?",
+            "rating": null
+        },
+        {
+            "answer": "Uruguay",
+            "category": 6,
+            "difficulty": 4,
+            "id": 11,
+            "question": "Which country won the first ever soccer World Cup in 1930?",
+            "rating": null
+        },
+        {
+            "answer": "George Washington Carver",
+            "category": 4,
+            "difficulty": 2,
+            "id": 12,
+            "question": "Who invented Peanut Butter?",
+            "rating": null
+        },
+        {
+            "answer": "Lake Victoria",
+            "category": 3,
+            "difficulty": 2,
+            "id": 13,
+            "question": "What is the largest lake in Africa?",
+            "rating": null
+        },
+        {
+            "answer": "Agra",
+            "category": 3,
+            "difficulty": 2,
+            "id": 15,
+            "question": "The Taj Mahal is located in which Indian city?",
+            "rating": null
+        }
+    ],
+    "success": true,
+    "total_questions": 20
+
+}
+
+```
+
+#### POST /questions
+- General:
+    - Creates a new questions using the submitted question, answer, difficulty, category and rating. 
+    - Returns the id of the created question, success value, total question, and questions list based on current page number to update the frontend. 
+     
+- `curl http://127.0.0.1:5000/questions?page=1 -X POST -H "Content-Type: application/json" -d '{"question":"Which is the largest country in Africa", "answer":"Algeria", "difficulty":"4","category":"3", "rating":"5"}'`
+```
+{
+  "created": 37, 
+  "questions": [
+    {
+      "answer": "Apollo 13", 
+      "category": 5, 
+      "difficulty": 4, 
+      "id": 2, 
+      "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Tom Cruise", 
+      "category": 5, 
+      "difficulty": 4, 
+      "id": 4, 
+      "question": "What actor did author Anne Rice first denounce, then praise in the role of her beloved Lestat?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Maya Angelou", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 5, 
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Edward Scissorhands", 
+      "category": 5, 
+      "difficulty": 3, 
+      "id": 6, 
+      "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Brazil", 
+      "category": 6, 
+      "difficulty": 3, 
+      "id": 10, 
+      "question": "Which is the only team to play in every soccer World Cup tournament?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Uruguay", 
+      "category": 6, 
+      "difficulty": 4, 
+      "id": 11, 
+      "question": "Which country won the first ever soccer World Cup in 1930?", 
+      "rating": null
+    }, 
+    {
+      "answer": "George Washington Carver", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 12, 
+      "question": "Who invented Peanut Butter?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Lake Victoria", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 13, 
+      "question": "What is the largest lake in Africa?", 
+      "rating": null
+    }, 
+    {
+      "answer": "Agra", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 15, 
+      "question": "The Taj Mahal is located in which Indian city?", 
+      "rating": null
+    }
+  ], 
+  "success": true, 
+  "total_questions": 21
+}
+```
+#### DELETE /questions/{question_id}
+- General:
+    - Deletes the question of the given ID if it exists. Returns the success value to update the frontend. 
+- `curl -X DELETE http://127.0.0.1:5000/questions/5`
+```
+{ 
+  "success": true
+}
+```
+
+#### GET /categories/1/questions
+- General:
+    - Returns a list of question objects, success value, and total number of questions, current_category for a specific category id 
+    - Results are paginated in groups of 10. Include a request argument to choose page number, starting from 1. 
+- Sample: `curl http://127.0.0.1:5000/categories/1/questions`
+
+```{
+
+    "current_category": "Science",
+    "questions": [
+        {
+            "answer": "The Liver",
+            "category": 1,
+            "difficulty": 4,
+            "id": 20,
+            "question": "What is the heaviest organ in the human body?",
+            "rating": null
+        },
+        {
+            "answer": "Alexander Fleming",
+            "category": 1,
+            "difficulty": 3,
+            "id": 21,
+            "question": "Who discovered penicillin?",
+            "rating": null
+        },
+        {
+            "answer": "Blood",
+            "category": 1,
+            "difficulty": 4,
+            "id": 22,
+            "question": "Hematology is a branch of medicine involving the study of what?",
+            "rating": null
+        },
+        {
+            "answer": "Maureen",
+            "category": 1,
+            "difficulty": 1,
+            "id": 31,
+            "question": "What is your name",
+            "rating": null
+        }
+    ],
+    "success": true,
+    "total_questions": 4
+}
+```
+
+
+#### POST /quizzes
+- General:
+    -  Gets questions to play the quiz. It takes the category and previous question parameters and return a random questions within the given category, if provided, and that is not one of the previous questions. 
+  
+    - Returns one questions details and a success value
+- `curl http://127.0.0.1:5000/quizzes -X POST -H "Content-Type: application/json" -d '{"previous_questions":[13,15],"quiz_category":{"type":"Science","id":"1"}}'`
+
+```{
+  "question": {
+    "answer": "The Liver", 
+    "category": 1, 
+    "difficulty": 4, 
+    "id": 20, 
+    "question": "What is the heaviest organ in the human body?", 
+    "rating": null
+  }, 
+  "success": true
+}
+```
+
+
+#### POST /categories
+- General:
+    - Creates a new category using the submitted type. 
+    - Returns the id of the created category id, success value, total number of categories, and categories list.
+- `curl http://127.0.0.1:5000/categories -X POST -H "Content-Type: application/json" -d '{"type":"Test 3"}'`
+   
+```
+{
+  "categories": [
+    {
+      "id": 1, 
+      "type": "Science"
+    }, 
+    {
+      "id": 2, 
+      "type": "Art"
+    }, 
+    {
+      "id": 3, 
+      "type": "Geography"
+    }, 
+    {
+      "id": 4, 
+      "type": "History"
+    }, 
+    {
+      "id": 5, 
+      "type": "Entertainment"
+    }, 
+    {
+      "id": 6, 
+      "type": "Sports"
+    }, 
+    {
+      "id": 7, 
+      "type": "Test"
+    }, 
+    {
+      "id": 8, 
+      "type": "Test 3"
+    }
+  ], 
+  "created": 8, 
+  "success": true, 
+  "total_categories": 8
+}
+
+```
--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -9,342 +9,256 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
- 
-  cors = CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-  
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
 
- 
+    cors = CORS(app,
+                resources={r"/*": {"origins": "*"}},
+                supports_credentials=True)
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
-
-  @app.after_request
-  def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers','Content-Type, Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    return response
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
-
-  @app.route('/categories')
-  def getcategories():
-    try:
-     categories= Category.query.order_by(Category.id).all()
-  
-     categorylist = [category.format() for category in categories]
-
-     if categories is None:
-       abort(404)
-
-     return jsonify({
-       'success':True,
-       'categories': get_category_list(),
-       'no_of_categories': len(categorylist)
-     })
-    except:
-      abort(422)
-
-     
-
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization,true')
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET,POST,PUT,DELETE,OPTIONS')
+        return response
 
 
-  def get_category_list():
-    categories = {}
-    for category in Category.query.all():
-        categories[category.id] = category.type
-    return categories
+#  to fetch categories which are returned in the questions endpoint
 
-  @app.route('/questions')
-  def getquestions():
-    questions = Question.query.order_by(Question.id).all()
-    current_questions=  paginate_questions(request,questions)
 
-    if len(current_questions) == 0:
-      abort(404)
+    def get_category_list():
+        categories = {}
+        for category in Category.query.all():
+            categories[category.id] = category.type
+        return categories
 
-    return jsonify({
+# GET requests for questions, the endpoint includes pagination (every 10 questions).
+# This endpoint returns a list of questions, number of total questions,
+# current category, categories.
+    @app.route('/questions')
+    def getquestions():
+        questions = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, questions)
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'questions': current_questions,
+            'categories': get_category_list(),
+            'total_questions': len(Question.query.all()),
+            'current_category': None
+        })
+
+    # Used to DELETE question using a question ID.
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.filter(
+                Question.id == question_id).one_or_none()
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify({
+                'success': True
+            })
+
+        except BaseException:
+            abort(422)
+   
+    # POST a new question,  which will require the question and answer text,
+    # category, and difficulty score. If a search term is provided this endpoint will return the search results
+
+    @app.route('/questions', methods=['POST'])
+    def add_question():
+        question = request.get_json()
+        new_question = question.get('question', None)
+        new_answer = question.get('answer', None)
+        new_difficulty = question.get('difficulty', None)
+        new_category = question.get('category', None)
+        new_rating = question.get('rating', None)
+        search_term = question.get('searchTerm', None)
+        try:
+
+            if search_term:
+                results = Question.query.order_by(
+                    Question.id).filter(
+                    Question.question.ilike(
+                        '%{}%'.format(search_term)))
+
+                current_questions = paginate_questions(request, results)
+                return jsonify({
                     'success': True,
                     'questions': current_questions,
-                    'categories': get_category_list(),
-                    'total_questions': len(Question.query.all()),
-                    'current_category': None
+                    'total_questions': len(results.all())
                 })
 
-  
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+            else:
+                questions = Question(question=new_question,
+                                     answer=new_answer,
+                                     difficulty=new_difficulty,
+                                     category=new_category,
+                                     rating=new_rating)
+                questions.insert()
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+                allquestions = Question.query.order_by(Question.id).all()
+                current_questions = paginate_questions(request, allquestions)
 
-  @app.route('/questions/<int:question_id>', methods=['DELETE'])
-  def delete_question(question_id):
-    try:
-      question =Question.query.filter(Question.id == question_id).one_or_none()
+                return jsonify({
+                    'success': True,
+                    'created': questions.id,
+                    'questions': current_questions,
+                    'total_questions': len(allquestions)
 
-      if question is None:
-        abort(404)
+                })
+        except BaseException:
+            abort(422)
 
-      question.delete()
-      # questions = Question.query.order_by(Question.id).all()
-      # current_questions = paginate_questions(request, question)
+# Gets the category name using the id
+    def getCategory(id):
+        category = Category.query.filter(Category.id == id).one_or_none()
 
-      return jsonify({
-        'success':True
-      })
-    
+        if category is None:
+            abort(404)
 
-    except:
-      abort(422)
+        return category.format()["type"]
 
+# GET endpoint to get questions based on category
+    @app.route('/categories/<int:category>/questions')
+    def get_questions_by_category(category):
+        questions = Question.query.filter(
+            Question.category == category).order_by(
+            Question.id).all()
+        current_questions = paginate_questions(request, questions)
 
-
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
-
-  @app.route('/questions', methods=['POST'])
-  def add_question():
-    question = request.get_json()
-    new_question = question.get('question', None)
-    new_answer = question.get('answer', None)
-    new_difficulty= question.get('difficulty', None)
-    new_category = question.get('category', None)
-    new_rating = question.get('rating', None)
-    search_term = question.get('searchTerm', None)
-    try:
-     
-      if search_term:
-        results = Question.query.order_by(Question.id).filter(Question.question.ilike('%{}%'.format(search_term)))
-        
-       
-        current_questions = paginate_questions(request, results)
+        if len(current_questions) == 0:
+            abort(404)
         return jsonify({
-          'success': True, 
-          'questions': current_questions,
-          'total_questions': len(results.all()) 
+            'success': True,
+            'questions': current_questions,
+            'total_questions': len(questions),
+            'current_category': getCategory(category)
         })
-      
-      else:
-        questions = Question(question=new_question,
-                            answer=new_answer, 
-                            difficulty=new_difficulty, 
-                            category= new_category,
-                            rating=new_rating)
-        questions.insert()
 
+# POST endpoint to get questions to play the quiz.
+# This endpoint takes category and previous question parameters  and
+# return a random questions within the given category, if provided, and
+# that is not one of the previous questions.
+    @app.route('/quizzes', methods=['POST'])
+    def play_quiz():
+        body = request.get_json()
 
-        allquestions = Question.query.order_by(Question.id).all()
-        current_questions =paginate_questions(request,allquestions)
+        previous_questions = body.get('previous_questions',None)
+        quiz_category = body.get('quiz_category', None)
+        print(len(previous_questions))
+        print(previous_questions)
 
-        return jsonify({
-          'success': True, 
-          'created':questions.id,
-          'questions': current_questions,
-          'total_questions': len(allquestions) 
+        try:
 
-        })
-    except:
-        abort(422)
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
-
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
-  def getCategory(id):
-    category = Category.query.filter(Category.id == id).one_or_none()
-
-    if category is None:
-      abort(404)
-   
-    return category.format()["type"]
-   
-  @app.route('/categories/<int:category>/questions')
-  def get_questions_by_category(category):
-      questions = Question.query.filter(Question.category == category).order_by(Question.id).all()
-      current_questions=  paginate_questions(request,questions)
-
-      if len(current_questions) == 0:
-         abort(404)
-      return jsonify({
-                      'success': True,
-                      'questions': current_questions,
-                      'total_questions': len(questions),
-                      'current_category': getCategory(category)
-                  })
-
-
-
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-  @app.route('/quizzes', methods=['POST'])
-  def play_quiz():
-    body= request.get_json()
-   
-    previous_questions = body.get('previous_questions')
-    quiz_category = body.get('quiz_category')
-
-    try:
-
-      if int(quiz_category.get('id'))==0: 
-        questions = Question.query.all()
-      elif int(quiz_category.get('id'))!=0 :
-        questions = Question.query.filter(Question.category == quiz_category.get('id')).all()
-      else:
-        abort(422)
-      selected = []
-      for question in questions:
-          if question.id not in previous_questions:
-              selected.append(question.format())
-              if len(selected) != 0:
+            if int(quiz_category.get('id')) == 0 & len(
+                    previous_questions) == 0:
+                questions = Question.query.all()
+            elif int(quiz_category.get('id')) != 0:
+                questions = Question.query.filter(
+                    Question.category == quiz_category.get('id')).all()
+            if not questions:
+                return abort(422)
+            selected = []
+            for question in questions:
+                if question.id not in previous_questions:
+                    selected.append(question.format())
+            if len(selected) != 0:
                 result = random.choice(selected)
                 return jsonify({
-                  'success': True,
-                  'question':result
+                    'success': True,
+                    'question': result
                 })
-              else:
-                  result={}
-                  return jsonify({
+            else:
+                return jsonify({
                     'success': False,
-                    'question':result
-                  })
-    except:
-      abort(400)
+
+                })
+        except Exception:
+            abort(422)
+
+# POST a new category,  which will require the category type
+    @app.route('/categories', methods=['POST'])
+    def add_category():
+        data = request.get_json()
+        new_type = data.get('type', None)
+
+        try:
+            category = Category(type=new_type)
+            category.insert()
+
+            allcategories = Category.query.order_by(Category.id).all()
+            categorylist = [category.format() for category in allcategories]
+
+            return jsonify({
+                'success': True,
+                'created': category.id,
+                'categories': categorylist,
+                'total_categories': len(allcategories)
+
+            })
+        except BaseException:
+            abort(422)
+
+# Used to paginate questions into 10 questions per page 
+    def paginate_questions(request, data):
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * QUESTIONS_PER_PAGE
+        end = start + QUESTIONS_PER_PAGE
+
+        questions = [question.format() for question in data]
+        current_questions = questions[start:end]
+
+        return current_questions
 
 
+# Error handlers for 400, 404, 422 and 500
 
-  @app.route('/categories', methods=['POST'])
-  def add_category():
-    data = request.get_json()
-    new_type = data.get('type', None)
-    
-    try:
-        category = Category(type=new_type)
-        category.insert()
-
-        allcategories = Category.query.order_by(Category.id).all()
-        categorylist = [category.format() for category in allcategories]
-
+    @app.errorhandler(400)
+    def bad_request(error):
         return jsonify({
-          'success': True, 
-          'created':category.id,
-          'categories':categorylist,
-          'total_categories': len(allcategories) 
+            'success': False,
+            'error': 400,
+            'message': 'bad request'
+        }), 400
 
-        })
-    except:
-        abort(422)
-   
- 
-  
-  def paginate_questions(request, data ):
-    page  = request.args.get('page',1, type=int)
-    start = (page-1)*QUESTIONS_PER_PAGE
-    end = start+QUESTIONS_PER_PAGE
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'server error'
+        }), 500
 
-    questions = [question.format() for question in data]
-    current_questions = questions[start:end]
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'resource not found'
+        }), 404
 
-    return current_questions
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable'
+        }), 422
 
-
-# 400, 404, 422 and 500
-    
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
-
-  @app.errorhandler(400)
-  def bad_request(error):
-    return jsonify({
-      'success':False,
-      'error':400,
-      'message': 'bad request'
-    }),400
-
-
-  @app.errorhandler(500)
-  def server_error(error):
-    return jsonify({
-      'success':False,
-      'error':500,
-      'message':'server error'
-    }),500
-
-  @app.errorhandler(404)
-  def not_found(error):
-    return jsonify({
-      'success':False,
-      'error':404,
-      'message': 'resource not found'
-    }), 404
-
-  @app.errorhandler(422)
-  def unprocessable(error):
-    return jsonify({
-      'success': False,
-      'error':422,
-      'message':'unprocessable'
-    }),422
-    
-  return app
-
-    
+    return app
